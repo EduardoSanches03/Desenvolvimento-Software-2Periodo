fce7f5935a783df08396177bed9bc19d1bf1772f
EduardoINtheTop <127109184+EduardoINtheTop@users.noreply.github.com>
2023-03-08 22:40:41 -0300

    Add files via upload

---
+++ _src/ws/WS/src/Lista02/Exe01.java
@@ -0,0 +1,29 @@
+package Lista02;
+
+import java.util.Scanner;
+
+public class Exe01 {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Informe o tamanho da sequencia");
+		int tamanho = leitor.nextInt();
+		
+		for (int i = 1; i <= tamanho; i++) {
+            System.out.print("Digite o " + i + "º número da sequência: ");
+            int numero = leitor.nextInt();
+            
+            if(numero > 0) {
+            	System.out.println(""+" Numero Positiovo");
+            }
+            else if(numero < 0) {
+            	System.out.println(""+" Numero Negativo");
+            }else {
+            	System.out.println(""+" Zero");
+            }
+		}
+		leitor.close();
+	}
+}
---
+++ _src/ws/WS/src/Lista02/Exe05.java
@@ -0,0 +1,30 @@
+package Lista02;
+
+import java.util.Scanner;
+
+public class Exe05 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int [] vetor = new int[5];
+		int n,i,q = 0;
+		
+		System.out.println("Informe um número qualquer: ");
+		n = leitor.nextInt();
+		
+		for(i = 0; i < vetor.length; i++) {
+			System.out.println("Informe o "+(i+1)+"° elemento");
+			vetor[i] = leitor.nextInt();
+			if(n == vetor[i]) {
+				q++;
+			}
+		}
+		
+		
+		System.out.println(""+q+" vezes o número se repetiu no vetor");
+		
+		
+		leitor.close();
+	}
+}
